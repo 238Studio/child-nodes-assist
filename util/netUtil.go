2a50e133d3e710762a56f7a0417a459def9ae419
RealCommonCat <128367240+RealCommonCat@users.noreply.github.com>
2023-11-01 21:21:37 +0800

    更新了网络包的数据格式

--- util/netUtil.go
+++ util/netUtil.go
@@ -3,10 +3,13 @@ package util
 // SendWebsocketNetMessage 包装了网络讯息发送类
 // 传入：需要发送的消息，核心的目标模块，是否是原始类型
 // 传出：无
-func SendWebsocketNetMessage(data *[]byte, targetModuleID string, isBytes bool) *map[string][]interface{} {
-	r := make(map[string][]interface{})
-	r[targetModuleID] = make([]interface{}, 2)
-	r[targetModuleID][0] = isBytes
-	r[targetModuleID][1] = *data
+func SendWebsocketNetMessage(data *[]byte, targetModuleID string, isBytes bool) *[]interface{} {
+	r := make([]interface{}, 3)
+	// 核心的目标模块ID
+	r[0] = targetModuleID
+	// 是否是原始二进制数据
+	r[1] = isBytes
+	// 数据
+	r[2] = *data
 	return &r
 }
