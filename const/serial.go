9e465bfd9a303be4cc41d32d37b034d2646075e2
RealCommonCat <128367240+RealCommonCat@users.noreply.github.com>
2023-11-25 12:50:59 +0800

    将子节点模块改为了uint32

--- const/serial.go
+++ const/serial.go
@@ -2,17 +2,17 @@ package _const
 
 // 数据报常数
 const (
-	PortHead   byte = 0xEF
-	PortEnd    byte = 0xFF
-	PortMaxLen      = 0xFFFF
+	PortHead   uint32 = 0xEF
+	PortEnd    uint32 = 0xFF
+	PortMaxLen uint32 = 0xFFFF
 )
 
 // 子节点模块常数
 const (
-	InitModule   byte = 0x00
-	SensorModule byte = 0x01
-	ReportModule byte = 0x02
-	ErrorModule  byte = 0x03
+	InitModule   uint32 = 0x00
+	SensorModule uint32 = 0x01
+	ReportModule uint32 = 0x02
+	ErrorModule  uint32 = 0x03
 )
 
 // 特殊数据报
@@ -22,5 +22,5 @@ const (
 
 // 特殊子节点模块
 const (
-	SerialVerify byte = 0x0F
+	SerialVerify uint32 = 0x0F
 )
