ddb9c02f5bb06d4cd50a832e8bc9dcf77c7b9ca9
RealCommonCat <128367240+RealCommonCat@users.noreply.github.com>
2023-11-26 00:41:41 +0800

    Update serial.go

--- const/serial.go
+++ const/serial.go
@@ -17,8 +17,8 @@ const (
 const (
 	// FailedToRev 接收失败
 	FailedToRev uint32 = 0xf0
-	// ReceiveSuccess 接收成功
-	ReceiveSuccess uint32 = 0xf1
+	// SuccessRev 接收成功
+	SuccessRev uint32 = 0xf1
 )
 
 // 特殊子节点模块
