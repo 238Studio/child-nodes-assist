f64dfcaa2d1f3abf175271179d68d053035a5884
RealCommonCat <128367240+RealCommonCat@users.noreply.github.com>
2023-12-06 16:58:38 +0800

    更新了模块 并修改了特殊数据报

--- const/serial.go
+++ const/serial.go
@@ -7,22 +7,24 @@ const (
 
 // 子节点模块常数
 const (
-	InitModule   uint32 = 0x00
+	// InitModule 初始化模块 用于处理下位机初始化相关的讯息
+	InitModule uint32 = 0x00
+	// SensorModule 传感器模块 用于处理下位机呈递的传感器讯息
 	SensorModule uint32 = 0x01
+	// ReportModule 日志报告模块 用于处理下位机呈递的日志
 	ReportModule uint32 = 0x02
-	ErrorModule  uint32 = 0x03
+	// ErrorModule 错误报告模块 用于处理下位机呈递的错误
+	ErrorModule uint32 = 0x03
+	// FeedbackModule 反馈模块 用于处理下位机的数据帧传递反馈要求 例如重传 并且下位机的同名模块用于接收传来的数据帧
+	FeedbackModule uint32 = 0x0f
 )
 
 // 特殊数据报
 const (
-	// FailedToRev 接收失败
-	FailedToRev uint32 = 0xf0
-	// SuccessRev 接收成功
-	SuccessRev uint32 = 0xf1
-	// ReadyResend 预备重发
-	ReadyResend uint32 = 0xf2
-	// CancelResend 取消重发
-	CancelResend uint32 = 0xf3
+	// InitData 初始化数据
+	InitData string = "InitData"
+	// ReSendData 重新发送的数据
+	ReSendData string = "ReSendData"
 )
 
 // 特殊子节点模块
