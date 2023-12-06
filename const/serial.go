eca4870d11bc5f74dd8576e79e99ca1e8be6e9a9
RealCommonCat <128367240+RealCommonCat@users.noreply.github.com>
2023-12-06 16:01:54 +0800

    Update serial.go
    更新了几个模块

--- const/serial.go
+++ const/serial.go
@@ -8,9 +8,15 @@ const (
 // 子节点模块常数
 const (
 	InitModule   uint32 = 0x00
+	// 初始化模块 用于处理下位机初始化相关的讯息
 	SensorModule uint32 = 0x01
+	// 传感器模块 用于处理下位机呈递的传感器讯息
 	ReportModule uint32 = 0x02
+	// 日志报告模块 用于处理下位机呈递的日志
 	ErrorModule  uint32 = 0x03
+	// 错误报告模块 用于处理下位机呈递的错误
+	FeedbackModule uint32 = 0x0f
+	// 反馈模块 用于处理下位机的数据帧传递反馈要求 例如重传 并且下位机的同名模块用于接收传来的数据帧
 )
 
 // 特殊数据报
